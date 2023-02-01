a2c92873bfa3f30f96f9a9c1580a305d9f04ad39
Muffin1488 <hsstrydom100@gmail.com>
2023-02-01 15:07:25 +0200

    build 1

---
+++ src/calculator.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.lang.*;
+import java.lang.Math;
+import java.util.Scanner;
+
+
+
+public class calculator {
+    public static void main(String[] args) {
+
+        double num1,  num2;
+        String operator;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter first number");
+        num1 = input.nextDouble();
+        System.out.println("enter operator");
+        operator = input.next();
+        System.out.println("enter second number");
+        num2 = input.nextDouble();
+
+        if(operator == "+"){
+            System.out.println(num1+num2);
+        } else if (operator == "-") {
+            System.out.println(num1-num2);
+        } else if (operator == "*") {
+            System.out.println(num1*num2);
+        } else if (operator == "/") {
+            System.out.println(num1/num2);
+        }
+
+
+    }
+}
